d227bcc00bdd7f4cfc3ba44abe3a73319c53af7e
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2024-10-23 13:24:48 +0200

    first commit

---
+++ src/main/java/com/pham_mishra/parisairline/ParisairlineApplication.java
@@ -0,0 +1,13 @@
+package com.pham_mishra.parisairline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParisairlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParisairlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pham_mishra/parisairline/ParisairlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pham_mishra.parisairline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParisairlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
